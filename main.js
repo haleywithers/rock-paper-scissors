d3f27bac8f70ba98de915eed0e203a57abebceb3
haleywithers <150790450+haleywithers@users.noreply.github.com>
2025-11-09 18:40:04 +0000

    Created a playRound function

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let humanScore = 0
+let computerScore = 0
+
 function getComputerChoice() {
     let randomNumber = Math.random();
 
@@ -15,5 +18,22 @@ function getHumanChoice() {
     return choice.toLowerCase();
 }
 
-let humanScore = 0
-let computerScore = 0
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!");
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You win! Rock beats Scissors");
+        humanScore++; // add 1 to human score
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You win! Paper beats Rock");
+        humanScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You win! Scissors beats Paper");
+        humanScore++;
+    } else {
+        console.log("You lose! " + computerChoice + " beats " + humanChoice);
+        computerScore++; // add 1 to computer score
+    }
+}
